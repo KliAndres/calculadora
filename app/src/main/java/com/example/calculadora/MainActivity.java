cfefef4cea50fd688537d9465419a1731ebd76b1
Carlos Pacheco <cali.pacheco0826@gmail.com>
2020-05-23 12:05:27 -0500

    Proyecto Terminado

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
+
+import static java.lang.Double.parseDouble;
 
 public class MainActivity extends AppCompatActivity {
     private EditText numero_uno, numero_dos;
     private TextView resultado;
+    private Spinner combo_operaciones;
+    private String[] operaciones;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +28,36 @@ public class MainActivity extends AppCompatActivity {
         numero_uno = findViewById(R.id.txtNumeroUno);
         numero_dos = findViewById(R.id.txtNumeroDos);
         resultado = findViewById(R.id.lblResultado);
+        combo_operaciones = findViewById(R.id.cmbOperaciones);
+        operaciones = getResources().getStringArray(R.array.operaciones);
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,operaciones);
+        combo_operaciones.setAdapter(adapter);
     }
     public void calcular(View v){
-        int num_uno, num_dos, suma;
+        int  op;
+        double num_uno, num_dos, res=0;
         if(validar()) {
-            num_uno = Integer.parseInt(numero_uno.getText().toString());
-            num_dos = Integer.parseInt(numero_dos.getText().toString());
-            suma = num_uno + num_dos;
-            resultado.setText("" + suma);
+            num_uno = parseDouble(numero_uno.getText().toString());
+            num_dos = parseDouble(numero_dos.getText().toString());
+            op = combo_operaciones.getSelectedItemPosition();
+
+            switch (op){
+                case 0:
+                    res = num_uno + num_dos;
+                    break;
+                case 1:
+                    res = num_uno - num_dos;
+                    break;
+                case 2:
+                    res = num_uno * num_dos;
+                    break;
+                case 3:
+                    res = num_uno / num_dos;
+                    break;
+            }
+
+            //suma = num_uno + num_dos;
+            resultado.setText("" + res);
         }
     }
     public void limpiar(View v){
@@ -34,17 +65,29 @@ public class MainActivity extends AppCompatActivity {
         numero_dos.setText("");
         resultado.setText("");
         numero_uno.requestFocus();
+        combo_operaciones.setSelection(0);
     }
     public boolean validar(){
-        String error_numero_uno, error_numero_dos;
+        String error_numero_uno, error_numero_dos, error_division;
+        int op;
+        double num_dos;
+
         error_numero_uno = getResources().getString(R.string.error_numero_uno);
         error_numero_dos = getResources().getString(R.string.error_numero_dos);
+        error_division = getResources().getString(R.string.error_division);
+
+         op= combo_operaciones.getSelectedItemPosition();
+
         if(numero_uno.getText().toString().isEmpty()){
             numero_uno.setError(error_numero_uno);
             numero_uno.requestFocus();
             return false;
         }else if(numero_dos.getText().toString().isEmpty()) {
             numero_dos.setError(error_numero_dos);
+            numero_dos.requestFocus();
+            return false;
+        }else if(op == 3 && Double.parseDouble(numero_dos.getText().toString()) ==0){
+            numero_dos.setError(error_division);
             numero_dos.requestFocus();
             return false;
         }
